3771493964e1e743914ab11207e39fcf2a140c95
Ht Developer <ganghuang@hengtiansoft.com>
2022-09-20 20:59:35 +0800

    maven project 初始化

---
+++ src/test/java/com/huanggang/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huanggang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
