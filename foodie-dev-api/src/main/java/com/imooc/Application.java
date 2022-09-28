dd87fda02b3c961bdb594dbfbe5143dda7ab2858
Ht Developer <ganghuang@hengtiansoft.com>
2022-09-28 21:47:05 +0800

    restful webservice 接口

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -3,11 +3,11 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 // 扫描 mybatis 通用 mapper 所在的包
-//@MapperScan(basePackages = "com.imooc.mapper")
-//@MapperScan(basePackages = "com.imooc.mapper")
+@MapperScan(basePackages = "com.imooc.mapper")
 // 扫描所有包以及相关组件包
 //@ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 //@EnableTransactionManagement
